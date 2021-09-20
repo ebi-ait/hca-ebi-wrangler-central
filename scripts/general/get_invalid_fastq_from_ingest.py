7dc92b1c766aebe8fd728abd33ecca096a872ff1
ESapenaVentura <38617863+ESapenaVentura@users.noreply.github.com>
2021-09-20 13:35:05 +0100

    Added an exception when there are no errored files

--- scripts/general/get_invalid_fastq_from_ingest.py
+++ scripts/general/get_invalid_fastq_from_ingest.py
@@ -22,29 +22,26 @@ if __name__ == '__main__':
     url = "https://api.ingest.archive.data.humancellatlas.org/submissionEnvelopes/search/findByUuidUuid?uuid="
     submission = rq.get(f"{url}{uuid}").json()
     files_page = rq.get(submission.get('_links').get('files').get('href')).json()
-    files = files_page['_embedded']['files']
-    while files_page.get('_links').get('next'):
-        files_page = rq.get(files_page.get('_links').get('next').get('href')).json()
-        files.extend(files_page.get('_embedded').get('files'))
-
     files = [file for file in files if file.get('validationErrors')]
-    filenames_list = [file['fileName'] for file in files]
-    with open('report_files.txt', 'w') as f:
-        f.write('\n'.join(filenames_list))
-
-
-    # Move invalid fastq files into a new 'filtered' folder to enable new download of files which were invalid into the current directory.
-    # The invalid files in the 'filtered' folder can then be optionally deleted.
-
-    os.mkdir('filtered')
-
-    with open('report_files.txt') as f:
-        mylist = f.read().splitlines()
-
-    fastq = glob.glob("*.fastq.gz")
-
-    for line in mylist:
+    if files:
+        filenames_list = [file['fileName'] for file in files]
+        with open('report_files.txt', 'w') as f:
+            f.write('\n'.join(filenames_list))
+    
+        # Move invalid fastq files into a new 'filtered' folder to enable new download of files which were invalid into the current directory.
+        # The invalid files in the 'filtered' folder can then be optionally deleted.
+    
+        os.mkdir('filtered')
+    
+        with open('report_files.txt') as f:
+            mylist = f.read().splitlines()
+    
+        fastq = glob.glob("*.fastq.gz")
+    
+        for line in mylist:
             if str(line) in fastq:
-                    shutil.move(line, os.path.join('filtered',line))
+                shutil.move(line, os.path.join('filtered', line))
             else:
-                    continue
+                continue
+    else:
+        print("No invalid files found")
