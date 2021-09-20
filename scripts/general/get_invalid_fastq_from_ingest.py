2d158001618205d73ded78112ec34cecc3a789f7
ESapenaVentura <38617863+ESapenaVentura@users.noreply.github.com>
2021-09-20 13:22:47 +0100

    Added argument parser to indicate UUID

--- scripts/general/get_invalid_fastq_from_ingest.py
+++ scripts/general/get_invalid_fastq_from_ingest.py
@@ -3,36 +3,44 @@ import os
 import sys
 import glob
 import shutil
+import argparse
 
-# List the names of the fastq files that are invalid given a submission uuid.
 
-uuid = '20f37aaf-caa1-40e6-9123-be6ce8feb2d6'
-url = "https://api.ingest.archive.data.humancellatlas.org/submissionEnvelopes/search/findByUuidUuid?uuid="
-submission = rq.get(f"{url}{uuid}").json()
-files_page = rq.get(submission.get('_links').get('files').get('href')).json()
-files = files_page['_embedded']['files']
-while files_page.get('_links').get('next'):
-    files_page = rq.get(files_page.get('_links').get('next').get('href')).json()
-    files.extend(files_page.get('_embedded').get('files'))
-
-files = [file for file in files if file.get('validationErrors')]
-filenames_list = [file['fileName'] for file in files]
-with open('report_files.txt', 'w') as f:
-    f.write('\n'.join(filenames_list))
-
+if __name__ == '__main__':
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--submission-uuid', help='Submission UUID', required=True, type=str)
+    args = parser.parse_args()
     
-# Move invalid fastq files into a new 'filtered' folder to enable new download of files which were invalid into the current directory.
-# The invalid files in the 'filtered' folder can then be optionally deleted.
+    # List the names of the fastq files that are invalid given a submission uuid.
 
-os.mkdir('filtered')
+    uuid = args.submission_uuid
+    url = "https://api.ingest.archive.data.humancellatlas.org/submissionEnvelopes/search/findByUuidUuid?uuid="
+    submission = rq.get(f"{url}{uuid}").json()
+    files_page = rq.get(submission.get('_links').get('files').get('href')).json()
+    files = files_page['_embedded']['files']
+    while files_page.get('_links').get('next'):
+        files_page = rq.get(files_page.get('_links').get('next').get('href')).json()
+        files.extend(files_page.get('_embedded').get('files'))
 
-with open('report_files.txt') as f:
-    mylist = f.read().splitlines()
+    files = [file for file in files if file.get('validationErrors')]
+    filenames_list = [file['fileName'] for file in files]
+    with open('report_files.txt', 'w') as f:
+        f.write('\n'.join(filenames_list))
 
-fastq = glob.glob("*.fastq.gz")
 
-for line in mylist:
-        if str(line) in fastq:
-                shutil.move(line, os.path.join('filtered',line))
-        else:
-                continue
+    # Move invalid fastq files into a new 'filtered' folder to enable new download of files which were invalid into the current directory.
+    # The invalid files in the 'filtered' folder can then be optionally deleted.
+
+    os.mkdir('filtered')
+
+    with open('report_files.txt') as f:
+        mylist = f.read().splitlines()
+
+    fastq = glob.glob("*.fastq.gz")
+
+    for line in mylist:
+            if str(line) in fastq:
+                    shutil.move(line, os.path.join('filtered',line))
+            else:
+                    continue
